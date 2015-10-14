6f8265f886586fcb28c25db0de8f4e6d47dcbc6b
MarcusBergholm <marcusbergholm@hotmail.com>
2015-10-14 17:43:42 +0200

    Initial Commit

---
+++ lab2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab2
+//
+//  Created by Marcus Bergholm on 14/10/15.
+//  Copyright © 2015 Marcus Bergholm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
